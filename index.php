6af7192e3f178b53316500a7e9ee168978e57f55
ramadhanswalleh <ramadhan.swalleh@evidenceaction.org>
2018-05-31 15:57:50 +0000

    index files commit.

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Evidence Action</title>
+
+  </head>
+
+<body style="background:#cccccc;width:100%;">
+
+  <div style="float: left;background:#FFFFFF;width:100%;"><img src="dtw/images/logo.png" />    </div>
+
+  <br/>       
+
+  <table style="margin-top:10%;margin-left:30%;">
+
+    <tr>
+
+    <td width="50%">
+
+      <a href="dtw/" style="text-decoration:none;font-size:1.8em;color:rgb(255,255,255); text-align:center;" >
+
+        <div style="background-color: #202640; width: 250px; padding-bottom:15px;border-radius: 10px;" onMouseOut="this.style.backgroundColor = '#202640'" onMouseOver="this.style.backgroundColor = '#1A213F'">
+
+          <p style="margin-bottom: 10px;" >DTW</p>
+
+          <img align="middle"src="dtw/images/kenya.png" height="250px"/>
+
+        </div>
+
+      </a>
+
+    </td>
+
+    <td width="50%">
+
+      <a href="dsw/" style="text-decoration:none;font-size:1.8em;color:rgb(255,255,255); text-align:center;">
+
+        <div style="background-color: #2E6889; width: 250px; padding-bottom:15px;border-radius: 10px;" onMouseOut="this.style.backgroundColor = '#2E6889'" onMouseOver="this.style.backgroundColor = '#24526B'">
+
+          <p style="margin-bottom: 10px;" >DSW</p>
+
+          <img align="middle" src="dtw/images/eastafrica.png" height="250px"/>
+
+        </div>
+
+      </a>
+
+    </td>
+
+    </tr>
+
+  </table>
+
+</body>
+
+</html>
---
+++ index_maintenance.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Evidence Action</title>
+    <style type="text/css">
+<!--
+.style1 {
+	color: #FF3366;
+	font-weight: bold;
+}
+-->
+    </style>
+</head>
+
+<body style="background:#cccccc; width:100%; color: #000000;">
+
+<div style="float: left;background:#FFFFFF;width:100%;"><img src="dtw/images/logo.png" />    </div>
+
+  <br/>
+  <p align="center">&nbsp;</p>
+  <p align="center">&nbsp;</p>
+  <p align="center">&nbsp;</p>
+  <p align="center">&nbsp;</p>
+  <p align="center"><a href="file:///C|/Users/EvidenceAction1/Documents/IS/Database Back-ups/PROGMIS/dtw" style="text-decoration:none;font-size:1.8em;color:rgb(255,255,255); text-align:center;">HELLO!</a></p>
+  <p align="center"><a href="file:///C|/Users/EvidenceAction1/Documents/IS/Database Back-ups/PROGMIS/dtw" style="text-decoration:none;font-size:1.8em;color:rgb(255,255,255); text-align:center;">PROGMIS is currently down for maintenance and will be up tomorrow morning. </a></p>
+  <p align="center"><a href="file:///C|/Users/EvidenceAction1/Documents/IS/Database Back-ups/PROGMIS/dtw" style="text-decoration:none;font-size:1.8em;color:rgb(255,255,255); text-align:center;">We apologise for any inconvenience cause.</p>
+    <p align="center"><a href="file:///C|/Users/EvidenceAction1/Documents/IS/Database Back-ups/PROGMIS/dtw" style="text-decoration:none;font-size:1.8em;color:rgb(255,255,255); text-align:center;">THANK YOU!</a></p>
+</body>
+
+</html>
